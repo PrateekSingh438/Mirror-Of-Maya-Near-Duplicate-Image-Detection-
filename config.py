900f2cfb36ae5a26d4bbb1a685f20ae930ffb89a
Prateek Singh <ps581.cse28@gmail.com>
2026-01-15 15:30:59 +0530

    config file added

---
+++ config.py
@@ -0,0 +1,9 @@
+import torch
+
+DATASET_PATH = ""     # Local Data path 
+MODEL_ID = "facebook/dinov2-small" #Change small to base for better accuracy
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 32 #change to 64 if better GPU is available
+SIMILARITY_THRESHOLD = 0.92
+HASH_SIZE = 8
+HASH_THRESHOLD = 2
